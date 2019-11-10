50d72e1b882818273cbaa17ad1a2bd7b07bb6137
Peter Larkin <G00332346@gmit.ie>
2019-11-10 23:17:08 +0000

    Jar errors fixed

--- PasswordService/target/generated-sources/protobuf/java/ie/gmit/ds/Inventory.java
+++ PasswordService/target/generated-sources/protobuf/java/ie/gmit/ds/Inventory.java
@@ -39,23 +39,21 @@ public final class Inventory {
   static {
     java.lang.String[] descriptorData = {
       "\n\017inventory.proto\022\nie.gmit.ds\032\036google/pr" +
-      "otobuf/wrappers.proto\032\033google/protobuf/e" +
-      "mpty.proto\"B\n\nHashOutput\022\016\n\006userId\030\001 \001(\005" +
-      "\022\026\n\016hashedPassword\030\002 \001(\014\022\014\n\004salt\030\003 \001(\014\"-" +
-      "\n\tHashInput\022\016\n\006userId\030\001 \001(\005\022\020\n\010password\030" +
-      "\002 \001(\t\"G\n\rValidateInput\022\020\n\010password\030\001 \001(\t" +
-      "\022\026\n\016hashedPassword\030\002 \001(\014\022\014\n\004salt\030\003 \001(\0142\213" +
-      "\001\n\017PasswordService\0225\n\004hash\022\025.ie.gmit.ds." +
-      "HashInput\032\026.ie.gmit.ds.HashOutput\022A\n\010val" +
-      "idate\022\031.ie.gmit.ds.ValidateInput\032\032.googl" +
-      "e.protobuf.BoolValueB\016\n\nie.gmit.dsP\001b\006pr" +
-      "oto3"
+      "otobuf/wrappers.proto\"B\n\nHashOutput\022\016\n\006u" +
+      "serId\030\001 \001(\005\022\026\n\016hashedPassword\030\002 \001(\014\022\014\n\004s" +
+      "alt\030\003 \001(\014\"-\n\tHashInput\022\016\n\006userId\030\001 \001(\005\022\020" +
+      "\n\010password\030\002 \001(\t\"G\n\rValidateInput\022\020\n\010pas" +
+      "sword\030\001 \001(\t\022\026\n\016hashedPassword\030\002 \001(\014\022\014\n\004s" +
+      "alt\030\003 \001(\0142\213\001\n\017PasswordService\0225\n\004hash\022\025." +
+      "ie.gmit.ds.HashInput\032\026.ie.gmit.ds.HashOu" +
+      "tput\022A\n\010validate\022\031.ie.gmit.ds.ValidateIn" +
+      "put\032\032.google.protobuf.BoolValueB\016\n\nie.gm" +
+      "it.dsP\001b\006proto3"
     };
     descriptor = com.google.protobuf.Descriptors.FileDescriptor
       .internalBuildGeneratedFileFrom(descriptorData,
         new com.google.protobuf.Descriptors.FileDescriptor[] {
           com.google.protobuf.WrappersProto.getDescriptor(),
-          com.google.protobuf.EmptyProto.getDescriptor(),
         });
     internal_static_ie_gmit_ds_HashOutput_descriptor =
       getDescriptor().getMessageTypes().get(0);
@@ -76,7 +74,6 @@ public final class Inventory {
         internal_static_ie_gmit_ds_ValidateInput_descriptor,
         new java.lang.String[] { "Password", "HashedPassword", "Salt", });
     com.google.protobuf.WrappersProto.getDescriptor();
-    com.google.protobuf.EmptyProto.getDescriptor();
   }
 
   // @@protoc_insertion_point(outer_class_scope)
